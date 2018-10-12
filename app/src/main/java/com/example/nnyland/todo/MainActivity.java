6a6428b5676c5d309b7f3aee2337d3051b7e94ea
Nelson Nyland <nelsonnyland@gmail.com>
2018-10-11 21:12:32 -0700

    Initial commit

---
+++ app/src/main/java/com/example/nnyland/todo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.nnyland.todo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity
+        implements OnClickListener {
+
+    private Button btnAddTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAddTask = (Button) findViewById(R.id.btnAddTask);
+        btnAddTask.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // create new task
+    }
+}
