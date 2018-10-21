19ffef592b74492fe3fe1eaa2eac9cf1ac02a6b3
Nelson Nyland <nelsonnyland@gmail.com>
2018-10-20 18:19:30 -0700

    Added onPause() and onResume() methods (commented out).

--- app/src/main/java/com/example/nnyland/todo/MainActivity.java
+++ app/src/main/java/com/example/nnyland/todo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nnyland.todo;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity
         implements OnClickListener {
 
+    private SharedPreferences savedValues;
     private Button btnAddTask;
 
     @Override
@@ -18,6 +21,7 @@ public class MainActivity extends Activity
         setContentView(R.layout.activity_main);
         btnAddTask = (Button) findViewById(R.id.btnAddTask);
         btnAddTask.setOnClickListener(this);
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
     }
 
     @Override
@@ -25,4 +29,22 @@ public class MainActivity extends Activity
         Toast.makeText(this, "Button Clicked",
                         Toast.LENGTH_LONG).show();
     }
+
+    /*
+    @Override
+    public void onPause() {
+        Editor editor = savedValues.edit();
+        editor.putString("task1", task1);
+        //editor.commit();
+        editor.apply();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        task1 = savedValues.getString("task1", "");
+        txtTask1.setText(task1);
+    }
+    */
 }
